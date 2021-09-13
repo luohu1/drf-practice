ed226b385ac754ec3c66ce678407ef2b975d396d
luohu1 <mylhmail@163.com>
2021-09-14 01:16:13 +0800

    feat: custom UserModel

--- app/account/models.py
+++ app/account/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    nickname = models.CharField(max_length=64, null=False, verbose_name=_('nickname'))
+    first_name = None
+    last_name = None
+
+    class Meta:
+        ordering = ['-pk']
+
+    def __str__(self) -> str:
+        return self.username
