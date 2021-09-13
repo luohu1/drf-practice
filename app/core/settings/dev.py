589dc8ee2cf05519d024ad2876eb68475f080b48
luohu1 <mylhmail@163.com>
2021-09-13 23:37:47 +0800

    feat: common settings

---
+++ app/core/settings/dev.py
@@ -0,0 +1,23 @@
+from .base import *  # NOQA
+from .base import INSTALLED_APPS
+
+DEBUG = True
+ALLOWED_HOSTS = ['*', ]
+LANGUAGE_CODE = 'zh-hans'
+TIME_ZONE = 'Asia/Shanghai'
+USE_TZ = False
+# AUTH_USER_MODEL = 'auth.User'
+
+INSTALLED_APPS += []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'NAME': 'drf_practices',
+        'PORT': 3306,
+        'CONN_MAX_AGE': 60
+    }
+}
